7b536a808588c69e45775f4b51e989d917c0d2f8
KevinSX31S <160195404+KevinSX31S@users.noreply.github.com>
2024-02-22 22:18:05 -0500

    Add files via upload

---
+++ crudestudiantes.php
@@ -0,0 +1,5 @@
+<?php
+require_once('estudiante');
+$Student = new Estudiante("Julio","Alarcon","julioalarcon@bcp.com.pe","941691210","Futbol");
+echo var_dump($Student);
+?>
---
+++ tables.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+</head>
+    <body>
+<?php
+$tablas_dinamicas = array{
+    array1("Nombres","Apellidos","Correo"),
+    array2("Damasco","Framburca","Naranja"),
+    array3("Kiwi","Pera","Uva"),
+};
+?>
+<table id="tabla" class="table table-bordered table-hover table-responsable">
+<thead>
+    <tr style = "width:1000px">
+    <th style = "width:45px"> num</th>
+    <th style = "width:400px"> Nombres</th>
+    <th style = "width:250px"> Usuario</th>
+    <th style = "width:250px"> Grupo</th>
+    <th colspan="2"style="width 150px">style</th>
+    </tr>
+</thead>
+</tbody>
+<tr style = "width: 1000px">
+<tr style = "width: 400px">I</td>
+<tr style = "width: 400px"><?php echo "Julio Cesar" ?></td>
+<tr style = "width: 400px">Administrador</td>
+<tr style = "width: 400px">Admin</td>
+<tr style = "width: 400px"></td>
+
+</table>
+</body>
+</html>
